5484ebff55719aba5a9692a72c8b488bd1bffd75
Kirill Trofin <kirill2977932@yandex.ru>
2024-11-12 21:14:14 +0300

    loggers added

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,18 +3,22 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class Util {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/user";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "root";
 
+    private static final Logger logger = Logger.getLogger(Util.class.getName());
+
     public static Connection getConnection() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+            logger.info("Connection established");
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.severe("Connection failed");
         }
         return connection;
     }
